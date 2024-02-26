05aee20a36c5df06a701766ef059cf0b766d0e33
vsalter <vsalter1@student.gsu.edu>
2024-02-26 02:20:20 -0500

    linked database and created initial tables

--- TheBlogSpot/TheBlogSpot/settings.py
+++ TheBlogSpot/TheBlogSpot/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TheBlogSpot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TheBlogSpot',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
