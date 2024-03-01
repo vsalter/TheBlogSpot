238737105b80a15882943bbf5977bfde2aa6f2db
vsalter <vsalter1@student.gsu.edu>
2024-02-29 23:10:23 -0500

    added Post model and migrated database

--- TheBlogSpot/main_app/views.py
+++ TheBlogSpot/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def home(request):
@@ -9,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def blog_posts(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', {'posts': posts})
