bbce2c6de70f880b83e82ff8d1e67953639e1795
vsalter <vsalter1@student.gsu.edu>
2024-02-29 23:21:31 -0500

    added to model so that title name shows in admin panel

--- TheBlogSpot/main_app/models.py
+++ TheBlogSpot/main_app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.title
